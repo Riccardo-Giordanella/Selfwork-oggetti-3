b5adcbc36720b2d243cae17a2c2ddfc02bed5b47
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-22 16:47:21 +0200

    Esercizio corretto

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let bowling = {
     players: [
-        { 'name': 'Livio', 'scores': [] },
-        { 'name': 'Paola', 'scores': [] },
-        { 'name': 'Filippo', 'scores': [] },
-        { 'name': 'Giuseppe', 'scores': [] }
+        // { 'name': 'Livio', 'scores': [] },
+        // { 'name': 'Paola', 'scores': [] },
+        // { 'name': 'Filippo', 'scores': [] },
+        // { 'name': 'Giuseppe', 'scores': [] }
     ],
 
     crea_punteggi_casuali: function () {
@@ -23,28 +23,37 @@ let bowling = {
 
 
     aggiungi_giocatore: function (nome) {
-        let nuovoGiocatore = { name: nome, scores: [], totalScore: 0 };
-        for (let i = 0; i < 10; i++) {
-            nuovoGiocatore.scores.push(Math.floor(Math.random() * (10 - 1 + 1) + 1));
-        }
-        nuovoGiocatore.totalScore = nuovoGiocatore.scores.reduce((acc, score) => acc + score, 0);
+        let nuovoGiocatore = { name: nome, scores: []};
         this.players.push(nuovoGiocatore);
     },
 
     determina_vincitore: function () {
-        this.trova_punteggio_finale();
-        return "Il vincitore è:", this.players[0];
+        return "Il vincitore è:"+ this.players[0].name;
     },
 
     classifica_finale: function () {
-        this.trova_punteggio_finale();
-        return this.players;
+        console.log('Classifica finale:');
+        this.players.forEach( (player, i)=>{
+            console.log(`${i + 1}. ${player.name}`);
+        })
+
     },
 }
 
-bowling.crea_punteggi_casuali(); // Crea 10 punteggi casuali per ogni giocatore
-console.log(bowling.players); // Mostra i giocatori con i punteggi casuali
-bowling.aggiungi_giocatore('Marco'); // Aggiunge un nuovo giocatore con 10 punteggi casuali
-console.log(bowling.players); // Mostra i giocatori aggiornati
-console.log(bowling.determina_vincitore()); // Determina il vincitore
-console.log(bowling.classifica_finale()); // Mostra la classifica finale dei giocatori
+// bowling.crea_punteggi_casuali(); // Crea 10 punteggi casuali per ogni giocatore
+// console.log(bowling.players); // Mostra i giocatori con i punteggi casuali
+bowling.aggiungi_giocatore('Alessandro');
+bowling.aggiungi_giocatore('Riccardo');
+bowling.aggiungi_giocatore('Matteo');
+bowling.crea_punteggi_casuali();
+bowling.trova_punteggio_finale();
+console.log(bowling.determina_vincitore());
+bowling.classifica_finale();
+
+
+console.log(bowling);
+
+
+// console.log(bowling.players); // Mostra i giocatori aggiornati
+// console.log(bowling.determina_vincitore()); // Determina il vincitore
+// console.log(bowling.classifica_finale()); // Mostra la classifica finale dei giocatori
